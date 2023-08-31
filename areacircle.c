485f49b760947345b47e5d1d587131818c1d61de
shashikumar33 <142645732+shashikumar33@users.noreply.github.com>
2023-08-31 15:51:53 +0530

    Add files via upload

---
+++ areacircle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int r ;
+    float pi=3.14 ;
+    printf("enter a value of radius");
+    scanf("%d",&r);
+    printf("area of circle: %f",pi*r*r);
+    return 0;
+}
